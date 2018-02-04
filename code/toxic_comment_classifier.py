732f80edb302c8af2e6eed4126f2042bd6581f57
ram1988 <ramnarayan.cse@gmail.com>
2018-02-04 20:48:51 +0800

    Update toxic_comment_classifier.py

--- code/toxic_comment_classifier.py
+++ code/toxic_comment_classifier.py
@@ -3,6 +3,8 @@ import pandas as pd
 import spacy
 import pickle
 
+from logistic_regression import LogisticRegressor
+
 nlp = spacy.load("en")
 
 def loadQuestionsFromTrainDF():
@@ -41,13 +43,28 @@ def lemmatize_test_text():
     lemmatized_comments = lemmatize_text(comments)
     pickle.dump(lemmatized_comments, open("lemmatized_test_dataframe.pkl","wb"))
 
-#def train_model():
-#cd     training_dataf
+def train_model():
+    training_data = pd.read_pickle("../data/lemmatized_dataframe.pkl")
+
+    train_features = training_data[0]
+
+    targets = []
+    targets.append(training_data[1])
+    targets.append(training_data[2])
+    targets.append(training_data[3])
+    targets.append(training_data[4])
+    targets.append(training_data[5])
+    targets.append(training_data[6])
+
+    logis = LogisticRegressor()
+    logis = logis.train_model(train_features,targets)
+
+    pickle.dump(logis,open("logistic_model.pkl","wb"))
 
 #46573-to be done since it was big doc
-lemmatize_test_text()
+#lemmatize_test_text()
 
-
+train_model()
 
 
 
